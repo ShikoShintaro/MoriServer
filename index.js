0917050bed120db6e081b8627ba856444c41812e
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-05 20:03:44 +0800

    Ambot nlng gyud

--- index.js
+++ index.js
@@ -6,29 +6,12 @@ const cors = require("cors");
 
 const app = express();
 
-const auth = require("./routes/auth");
-const upload = require("./routes/upload");
-const chat = require("./routes/chat");
-
-console.log("AUTH TYPE:", typeof auth);
-console.log("UPLOAD TYPE:", typeof upload);
-console.log("CHAT TYPE:", typeof chat);
-
-app.use("/api/auth", auth);
-app.use("/api/upload", upload);
-app.use("/api/chat", chat);
-
 app.use(cors());
 app.use(express.json());
 
-
 const auth = require("./routes/auth");
 const upload = require("./routes/upload");
 const chat = require("./routes/chat");
-
-console.log("AUTH TYPE:", typeof auth);
-console.log("UPLOAD TYPE:", typeof upload);
-console.log("CHAT TYPE:", typeof chat);
 
 app.use("/api/auth", auth);
 app.use("/api/upload", upload);
@@ -37,6 +20,5 @@ app.use("/api/chat", chat);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Mongo Connected"))
     .catch(err => console.log(err));
-
 
 module.exports = app;
