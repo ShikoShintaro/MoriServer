fe9a169265dbeb3dee803d9c4bb2377855797ae6
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-05 20:40:10 +0800

    Heh

--- index.js
+++ index.js
@@ -24,13 +24,18 @@ async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI).then(m => m);
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      bufferCommands: false,
+    });
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
 }
 
-connectDB();
+// connect once (don’t block lambda)
+connectDB().catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
 
 module.exports = serverless(app);
