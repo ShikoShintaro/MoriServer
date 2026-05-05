67b7f4967fcf8112651d1a8a2925e883734439c5
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-05 19:50:24 +0800

    Debugs

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const cors = require("cors");
 
 const app = express();
 
+const auth = require("./routes/auth");
+const upload = require("./routes/upload");
+const chat = require("./routes/chat");
+
+console.log("AUTH TYPE:", typeof auth);
+console.log("UPLOAD TYPE:", typeof upload);
+console.log("CHAT TYPE:", typeof chat);
+
+app.use("/api/auth", auth);
+app.use("/api/upload", upload);
+app.use("/api/chat", chat);
+
 app.use(cors());
 app.use(express.json());
 
@@ -18,5 +30,6 @@ app.use("/api/chat", require("./routes/chat"));
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Mongo Connected"))
     .catch(err => console.log(err));
+
 
 module.exports = app;
