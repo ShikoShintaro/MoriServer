e2b7199ef20362608ade637afe409f9f123f62f2
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-05 20:17:53 +0800

    Kapoy na

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const serverless = require("serverless-http");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 
-const auth = require("./routes/auth");
-const upload = require("./routes/upload");
-const chat = require("./routes/chat");
-
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", auth);
-app.use("/api/upload", upload);
-app.use("/api/chat", chat);
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/upload", require("./routes/upload"));
+app.use("/api/chat", require("./routes/chat"));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Mongo Connected"))
     .catch(err => console.log(err));
 
-module.exports = app;
+module.exports = serverless(app);
