bbe798b3247948352a8c4c52568709770a5b66bd
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-05 20:58:03 +0800

    kapoy naman

--- routes/upload.js
+++ routes/upload.js
@@ -3,25 +3,28 @@ const multer = require("multer");
 const cloudinary = require("../config/cloudinary");
 
 const router = express.Router();
-const storage = multer.memoryStorage();
+
+const storage = multer.diskStorage({
+  destination: "temp/",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
 const upload = multer({ storage });
 
 router.post("/", upload.single("image"), async (req, res) => {
-    try {
-        const result = await cloudinary.uploader.upload(req.file.path, {
-            folder: "profileImages",
-        });
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "profileImages",
+    });
 
-        console.log("CLOUDINARY URL:", result.secure_url);
+    res.json({ imageUrl: result.secure_url });
 
-        res.json({
-            imageUrl : result.secure_url
-        });
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error : err.message });
-    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
