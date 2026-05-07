219ef06f719bbc220fb0a5935402f5de2e3ada74
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-07 18:56:45 +0800

    YAWA

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,19 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, 
-
-    tls: {
-        rejectUnauthorized: false
-    }
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 const sendCode = async (to, code) => {
