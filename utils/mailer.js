f85d0536483270989dd615025dd73c6da864ecd2
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-07 18:50:28 +0800

    Transporter fix

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,19 +2,34 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
-    auth : {
-        user : process.env.EMAIL_USER,
-        pass : process.env.EMAIL_PASS
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, 
+
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
 const sendCode = async (to, code) => {
-    await transporter.sendMail({
-        from: "MORI AI TEAM",
-        to,
-        subject : "MORI AI Verification Code",
-        text : `Your Verification code is ${code}` 
-    })
-}
+    try {
+        await transporter.sendMail({
+            from: `"MORI AI TEAM" <${process.env.EMAIL_USER}>`,
+            to,
+            subject: "MORI AI Verification Code",
+            text: `Your Verification code is ${code}`
+        });
+
+        console.log("OTP email sent to:", to);
+    } catch (err) {
+        console.log("Email send error:", err);
+        throw err;
+    }
+};
 
 module.exports = sendCode;
