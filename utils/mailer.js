edff54655291e89494bd0070176b6a3304ab9679
ShikoShintaro <shikoshintaro21@gmail.com>
2026-05-08 01:45:43 +0800

    uhh another debug

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,31 +2,30 @@ const axios = require("axios");
 
 const sendCode = async (email, code, type) => {
     try {
-        const res = await axios.post(
-            "https://8000-01khvck0a4b5095r1210t1kstp.cloudspaces.litng.ai/send-otp",
-            {
-                email,
-                code,
-                type
-            }
-        );
-
-        console.log("FastAPI email sent:", res.data);
-
         console.log("SENDING TO FASTAPI:", {
             email,
             code,
             type
         });
 
+        const res = await axios.post(
+            "https://8000-01khvck0a4b5095r1210t1kstp.cloudspaces.litng.ai/send-otp",
+            { email, code, type },
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+        console.log("FastAPI SUCCESS:", res.data);
         return true;
 
     } catch (err) {
-        console.log("Email send error:", err.message);
+        console.log("FASTAPI ERROR STATUS:", err.response?.status);
+        console.log("FASTAPI ERROR DATA:", err.response?.data);
         return false;
     }
-
-
 };
 
 module.exports = sendCode;
